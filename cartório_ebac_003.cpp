df693e8debb1c20b6d3d7a0cf51f6fdb9c993cb7
Renato Assis <130713431+Rntassis@users.noreply.github.com>
2025-03-14 05:27:16 -0300

    Update-cartorio-ebac

--- cartório_ebac_003.cpp
+++ cartório_ebac_003.cpp
@@ -124,8 +124,9 @@ int main()
 		printf("Escolha a opção desejada do menu: \n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
-		printf("Opção: ");	//fim do menu
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
+		printf("Opção: "); // fim do menu
 		
 		scanf("%d", &opcao); //salvando informações
 					
@@ -143,7 +144,13 @@ int main()
 				
 			case 3:
 			deletar();
-			break;		
+			break;
+			
+			case 4:
+			printf("Obrigado por tulizar o sistema!\n");
+			return 0;
+			break;
+					
 			
 			default:
 				printf("Essa opção não está disponível! Tente novamente\n");
